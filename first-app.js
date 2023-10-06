76b966ca9f2b02ef25b97007f53d4fae2f999bc9
Naveen <sheoran19997@gmail.com>
2023-10-06 11:56:25 +0530

    intro to express

--- first-app.js
+++ first-app.js
@@ -1,1 +1,15 @@
-console.log("hello from  nodejs")
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+    console.log('in middleware');
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log('in  another middleware');
+    res.send('<h1> working fine </h1>');
+})
+app.listen(3000);
+
+
+
