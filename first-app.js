4b84093d8c1f7f94a6757a91fe4e3f64a2f44aaf
Naveen <sheoran19997@gmail.com>
2023-10-06 12:48:49 +0530

    parse req body and handle post route

--- first-app.js
+++ first-app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-app.use((req,res,next)=>{
-    console.log('in middleware');
-    next();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST" ><input type="text" name="title" ></input> <input type="text" name="size"></input><button type="submit"> addd product</button></form> ');
+
 })
 
-app.use((req,res,next)=>{
-    console.log('in  another middleware');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
     res.send('<h1> working fine </h1>');
 })
 app.listen(3000);
